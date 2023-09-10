ae224b78bed86b34f924133197a1d17f1b29a4da
Tina Homayouni <t.homayouni@vardastapp.com>
2023-09-10 18:14:54 +0330

    controller handle with param

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './services/app.service';
 
-@Controller()
+@Controller('tempConvertor') // Change the route to '/tempConvertor'
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get(':fahrenheit')
-  convertToCelsius(@Param('fahrenheit') fahrenheit: number): string {
+  @Get()
+  convertToCelsius(@Query('fahrenheit') fahrenheit: number): string {
     const celsiusTemp = this.appService.convertFahrenheitToCelsius(fahrenheit);
     return `Celsius Temperature: <b>${celsiusTemp}</b>°C`;
   }
